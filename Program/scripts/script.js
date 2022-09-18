9aeacdab3447dcd8ccffdf2e9f2136316f447c7c
Dovile Sukiene <do.leskauskaite@gmail.com>
2022-09-19 01:05:57 +0300

    Program page added

---
+++ Program/scripts/script.js
@@ -0,0 +1,15 @@
+//alert(“Here is the notification”);
+
+
+//let answer = confirm(“Are you sure you want to leave this page?");
+
+//let person = prompt("Please enter your name", "Harry
+//   Potter");
+//   if (person != null) {
+//     document.getElementById("demo").innerHTML =
+//     "Hello " + person + "! How are you today?";
+//}
+
+
+//history.back();//navigates to the previous page visited
+//history.forward();//navigates to the next page visited
